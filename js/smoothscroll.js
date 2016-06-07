f7f5b77a2db637806fdb63841c39697cac00c9dc
Kristoffer Mendoza <kristoffer_mlmendoza@yahoo.com>
2016-06-07 08:13:06 +0800

    Filler data

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -4,6 +4,7 @@ $(function() {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
+
         $('html, body').animate({
           scrollTop: target.offset().top
         }, 500);
