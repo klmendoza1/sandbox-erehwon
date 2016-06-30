e2134e31f64fc2e0fab9e7453fd15e999dd9c348
Denise Leviste <drleviste@gmail.com>
2016-06-30 12:28:44 -0400

    Added reply-to header to contact_form.php

--- _site/about/contact_form.php
+++ _site/about/contact_form.php
@@ -10,16 +10,18 @@ if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
         // To send HTML mail, the Content-type header must be set.
         $headers = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/plain; charset=iso-8859-1' . "\r\n";
-        $headers .= 'From:' . $email . "\r\n"; // Sender's Email
+        $headers .= 'From: noreply@erehwonartfoundation.org' . "\r\n"; // Sender's Email
+        $headers .= 'Reply-To: ' . $email . "\r\n"; // Sender's Email
         // $headers .= 'Cc:' . $email . "\r\n"; // Carbon copy to Sender
         // $template    = '<div style="padding:50px; color:white;">Hello ' . $name . ',<br/>' . '<br/>Thank you...! For Contacting Us.<br/><br/>' . 'Name:' . $name . '<br/>' . 'Email:' . $email . '<br/>' . 'Contact No:' . $contact . '<br/>' . 'Message:' . $message . '<br/><br/>' . 'This is a Contact Confirmation mail.' . '<br/>' . 'We Will contact You as soon as possible .</div>';
-        $template    = 'Name: ' . $name . "\r\n" . 'Email: ' . $email . "\r\n" . "Message:\r\n" . $message . "\r\n";        
+        // $template    = 'Name: ' . $name . "\r\n" . 'Email: ' . $email . "\r\n" . "Message:\r\n" . $message . "\r\n";        
+        $template    = $message . "\r\n";        
         $sendmessage = $template;
         // Message lines should not exceed 70 characters (PHP rule), so wrap it.
         $sendmessage = wordwrap($sendmessage, 70);
         // Send mail by PHP Mail Function.
-        // mail("-finfo@erehwonartfoundation.org", $subject, $sendmessage, $headers);
-        mail("erehwonartfoundation@gmail.com", $subject, $sendmessage, $headers);
+        mail("info@erehwonartfoundation.org", $subject, $sendmessage, $headers);
+        //mail("klangga@gmail.com", $subject, $sendmessage, $headers);
         echo "Your query has been received, we will contact you soon.";
 } else {
     echo "<span>* invalid email *</span>";
